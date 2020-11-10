75474516cc5109c64da0cae6742a2f3bb830b01a
raguilar27 <rogelioaguilar208@gmail.com>
2020-11-09 23:37:55 -0800

    Inserted values into matches table

--- Queries.sql
+++ Queries.sql
@@ -6,3 +6,33 @@ DELETE FROM stats;
 DELETE FROM playerStats;
 DELETE FROM nation;
 
+/*importing matches for 2010/2011 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +90 data/11champs.csv' matches
+
+/*importing matches for 2011/2012 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +90 data/12champs.csv' matches
+
+/*importing matches for 2012/2013 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +90 data/13champs.csv' matches
+
+/*importing matches for 2013/2014 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +90 data/14champs.csv' matches
+
+/*importing matches for 2014/2015 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +92 data/15champs.csv' matches
+
+/*importing matches for 2015/2016 season*/
+.mode "csv"
+.separator ","
+.import '| tail -n +94 data/16champs.csv' matches
+
