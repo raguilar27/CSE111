0b6b3121235371d71846c8d87c40f9b99f36b08b
raguilar27 <rogelioaguilar208@gmail.com>
2020-12-07 23:31:00 -0800

    Created js script for queries

---
+++ ucl.js
@@ -0,0 +1,33 @@
+const sqlite3 = require('sqlite3')
+const Promise = require('bluebird')
+
+
+class UCL {
+    constructor(dbFilePath) {
+        this.db = new sqlite3.Database(dbFilePath, (err) => {
+            if (err) {
+                console.log('Could not connect to database', err)
+            } else {
+                console.log('Connected to database')
+            }
+        })
+    }
+
+    all(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, rows) => {
+                if (err) {
+                    console.log('Error running sql: ' + sql)
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
+    }
+
+    
+}
+
+module.exports = UCL
