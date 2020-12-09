fffeb808f5077374c0c1ec4b01609b73b307a72f
raguilar27 <rogelioaguilar208@gmail.com>
2020-12-09 14:55:24 -0800

    uploaded files for java app

---
+++ ucl.java
@@ -0,0 +1,75 @@
+import java.sql.*;
+
+public class ucl{
+    private Connection c = null;
+    private String dbName;
+    private boolean isConnected = false;
+
+    private void openConnection(String _dbName) {
+        dbName = _dbName;
+
+        if (false == isConnected) {
+            System.out.println("++++++++++++++++++++++++++++++++++");
+            System.out.println("Open database: " + _dbName);
+
+            try {
+                String connStr = new String("jdbc:sqlite:");
+                connStr = connStr + _dbName;
+
+                // STEP: Register JDBC driver
+                Class.forName("org.sqlite.JDBC");
+
+                // STEP: Open a connection
+                c = DriverManager.getConnection(connStr);
+
+                // STEP: Diable auto transactions
+                c.setAutoCommit(false);
+
+                isConnected = true;
+                System.out.println("success");
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+
+            System.out.println("++++++++++++++++++++++++++++++++++");
+        }
+    }
+
+    private void closeConnection() {
+        if (true == isConnected) {
+            System.out.println("++++++++++++++++++++++++++++++++++");
+            System.out.println("Close database: " + dbName);
+
+            try {
+                // STEP: Close connection
+                c.close();
+
+                isConnected = false;
+                dbName = "";
+                System.out.println("success");
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+
+            System.out.println("++++++++++++++++++++++++++++++++++");
+        }
+    }
+
+    
+
+    public static void main(String args[]) {
+        ucl sj = new ucl();
+        
+        sj.openConnection("data/data.sqlite");
+
+        sj.Q1();
+        sj.Q2();
+        sj.Q3();
+        sj.Q4();
+        sj.Q5();
+
+        sj.closeConnection();
+    }
+}
