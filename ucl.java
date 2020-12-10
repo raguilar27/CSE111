150166063425794b75cab0d3e24a2c00601805f4
raguilar27 <rogelioaguilar208@gmail.com>
2020-12-09 17:42:20 -0800

    added first query

--- ucl.java
+++ ucl.java
@@ -63,157 +63,35 @@ public class ucl{
         System.out.println("ALL GROUP STAGE MATCHES");
 
         try{
-            "SELECT m_group, m_date, m_team1, m_FT, m_team2 " +
-            "FROM matches " +
-            "WHERE m_stage = 'Group' " +
-                "AND m_seasonkey = ? " +
-            "ORDER BY m_group";
+            String sql = "SELECT m_group, m_date, m_team1, m_FT, m_team2 " +
+                "FROM matches " +
+                "WHERE m_stage = 'Group' " +
+                    "AND m_seasonkey = ? " +
+                "ORDER BY m_group";
+            
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setInt(1, _seasonkey);
+            ResultSet rs = stmt.executeQuery();
 
+            System.out.printf("%s|%s|%s|%s|%s\n", "Group", "Date", "HOME", "Full-Time", "AWAY");
+
+            while(rs.next()){
+                String group = rs.getString("m_group");
+                String date = rs.getString("m_date");
+                String home = rs.getString("m_team1");
+                String ft = rs.getString("m_FT");
+                String away = rs.getString("m_team2");
+
+                System.out.printf("%s|%s|%s|%s|%s\n", group, date, home, ft, away);
+            }
+
+            stmt.close();
+            rs.close();
         }catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
         System.out.println("++++++++++++++++++++++++++++++++++");
     }
-
-    matches16(_seasonkey){
-        return this.all(
-            "SELECT m_round, m_date, m_team1, m_FT, m_team2, m_agg, m_comments " +
-            "FROM matches " +
-            "WHERE m_stage = 'Knockout' " +
-                "AND m_seasonkey = ? " +
-                "AND m_round LIKE 'Round of 16%'", [_seasonkey])
-    }
-
-    matchesQuarter(_seasonkey){
-        return this.all(
-            "SELECT m_round, m_date, m_team1, m_FT, m_team2, m_agg, m_comments " +
-            "FROM matches " +
-            "WHERE m_stage = 'Knockout' " +
-                "AND m_seasonkey = ? " +
-                "AND m_round LIKE 'Quarterfinals%'", [_seasonkey])
-    }
-
-    matchesSemi(_seasonkey){
-        return this.all(
-            "SELECT m_round, m_date, m_team1, m_FT, m_team2, m_agg, m_comments " +
-            "FROM matches " +
-            "WHERE m_stage = 'Knockout' "+
-                "AND m_seasonkey = ? " +
-                "AND m_round LIKE 'Semifinals%'", [_seasonkey])
-    }
-
-    matchesFinal(_seasonkey){
-        return this.all(
-            "SELECT m_round, m_date, m_team1, m_FT, m_team2, m_agg, m_comments " +
-            "FROM matches " +
-            "WHERE m_stage = 'Knockout' " +
-                "AND m_seasonkey = ? " +
-                "AND m_round = 'Final'", [_seasonkey])
-    }
-
-    clubParticipants(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name " +
-            "FROM clubStats, nation, club " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "GROUP BY c_name", [_seasonkey])
-    }
-
-    csName(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY c_name", [_seasonkey])
-    }
-
-    csGamesPlayed(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_gamesPL DESC", [_seasonkey])
-    }
-
-    csGamesWon(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_gameW DESC", [_seasonkey])
-    }
-
-    csGamesTied(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " + 
-                "AND c_nationkey = n_nationkey" +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_gameT DESC", [_seasonkey])
-    }
-
-    csGamesLost(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_gameL DESC", [_seasonkey])
-    }
-
-    csGoalsFor(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_goalsFor DESC", [_seasonkey])
-    }
-
-    csGoalsAgainst(_seasonkey){
-        return this.all(
-            "SELECT c_name, n_name, cs_gamesPL, cs_gameW, cs_gameT, cs_gameL, cs_goalsFor, cs_goalsAgainst " +
-            "FROM clubStats, club, nation " +
-            "WHERE cs_clubkey = c_clubkey " +
-                "AND c_nationkey = n_nationkey " +
-                "AND cs_seasonkey = ? " +
-            "ORDER BY cs_goalsAgainst DESC", [_seasonkey])
-    }
-
-    psTopScorer(_seasonkey){
-        return this.all(
-            "SELECT p_name, p_goals, c_name, n_name " +
-            "FROM playerStats, club, nation " +
-            "WHERE p_nationkey = n_nationkey " +
-                "AND c_clubkey = p_clubkey " +
-                "AND p_seasonkey = ? " +
-            "ORDER BY p_goals DESC " +
-            "LIMIT 3", [_seasonkey])
-    }
-
-    psMostApperances(_seasonkey){
-        return this.all(
-            "SELECT p_name, p_goals, c_name, n_name " +
-            "FROM playerStats, club, nation " +
-            "WHERE p_nationkey = n_nationkey " +
-                "AND c_clubkey = p_clubkey " +
-                "AND p_seasonkey = ? " +
-            "ORDER BY p_apperances DESC " +
-            "LIMIT 3", [_seasonkey])
-    }
-
-    
 
     public static void main(String args[]) {
         ucl sj = new ucl();
@@ -232,6 +110,8 @@ public class ucl{
         System.out.println("For Season 14/15, type 15");
         System.out.println("For Season 15/16, type 16");
         _seasonkey = sc.nextInt();
+
+
 
 
 
